791d331257d62deb3e31c06a3fe7812d4d7135d3
piiivot <the_pivotsk8@hotmail.com>
2023-11-28 09:58:56 -0500

    feat : install config variable & config the DB

--- README.md
+++ README.md
@@ -4,7 +4,14 @@
 
 # Teslo API
 
-1. Levantar la base de datos
+1. Clonar proyecto
+2. ```npm i```
+3. Clonar el archivo ```.env.template``` y renombrarlo a ```.env```
+4. Cambiar las variables de entorno
+5. Levantar la base de datos
+
 ```
 docker-compose up -d
 ```
+
+6. Levantar: ```npm run start:dev```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // para que tome las variables de entorno
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      // ayuda a que si hay cambios en la bd de datos por ejemplo si se borra una colomna,
+      // con el synchronize se hacen el cambio en tiempo real
+      // en prod no se una el synchronize no se usa en true
+      synchronize: true,
+    })
+  ],
 })
 export class AppModule { }
