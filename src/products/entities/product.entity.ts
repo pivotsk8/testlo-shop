575d1e8758650b60b52699c6e51e94f07c6cb596
piiivot <the_pivotsk8@hotmail.com>
2023-11-28 10:55:39 -0500

    feat created entity products

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -10,6 +10,39 @@ export class Product {
     @Column('text', {
         unique: true,
     })
-    title: string
+    title: string;
+
+    @Column('numeric', {
+        default: 0
+    })
+    price: number;
+
+    @Column({
+        type: 'text',
+        nullable: true
+    })
+    description: string;
+
+    @Column('text', {
+        unique: true
+    })
+    slug: string;
+
+    @Column('int', {
+        default: 0
+    })
+    stock: number;
+
+    @Column('int', {
+        default: 0
+    })
+
+    @Column('text', {
+        array: true
+    })
+    size: string[];
+
+    @Column('text')
+    gender: string;
 }
 
