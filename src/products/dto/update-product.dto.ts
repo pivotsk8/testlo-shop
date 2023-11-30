f35bd4b3b22fc7d97407f73567575f6e0f29f3a3
piiivot <the_pivotsk8@hotmail.com>
2023-11-30 11:27:24 -0500

    fix github

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -2,4 +2,4 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
 
 //Expande todas la propiedades de CreateProductDto y las hace opcionales 
-export class UpdateProductDto extends PartialType(CreateProductDto) { }
+export class UpdateProductDto extends PartialType(CreateProductDto) { };
